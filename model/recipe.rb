dbb018ce7efcd6a8dbff5194d081d53424bb695b
Greg Lawson <glawson6@yahoo.com>
2015-01-29 18:59:05 -0500

    Updates for the app

--- model/recipe.rb
+++ model/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe
-  attr_accessor :id, :title, :directions, :ingredients, :cooking_time
+  attr_accessor :id, :title, :directions, :ingredients, :cooking_time, :image_url
   # Title, Directions, List of Ingredients, Cooking Time.
   def initialize(args)
     @id = args[:id]
@@ -7,5 +7,6 @@ class Recipe
     @directions = args[:directions]
     @ingredients = args[:ingredients]
     @cooking_time = args[:cooking_time]
+    @image_url = args[:image_url]
   end
 end
