83e5663c0c9fdb82598e6eb0908aa79c99abac66
Greg Lawson <glawson6@yahoo.com>
2015-01-29 15:14:32 -0500

    Making progress

--- app.rb
+++ app.rb
@@ -2,14 +2,17 @@ require 'sinatra'
 require_relative './model/cookbook'
 
 get '/' do
+  @active = "home"
   erb :home
 end
 
 get '/about' do
+  @active = "about"
   erb :about
 end
 
 get '/recipes' do
+  @active = "recipes"
   @search = params[:search]
   if @search && @search.length > 0
     @recipes = Cookbook.search(@search)
@@ -20,6 +23,7 @@ get '/recipes' do
 end
 
 get '/recipe/:id' do
+  @active = "recipes"
   @recipe = Cookbook.find(params[:id].to_i)
   erb :recipe
 end
